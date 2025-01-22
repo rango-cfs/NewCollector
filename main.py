0e64bfcb81cd78b7e9832d7df2c91e47d7ec4923
Rango_CF <118201907+rango-cfs@users.noreply.github.com>
2025-01-22 21:47:54 +0000

    Update My Code:   main.py Update Links:   v2ray_links.txt

--- main.py
+++ main.py
@@ -4,18 +4,26 @@ import re
 
 
 def fetch_v2ray_links(channel_url):
-    response = requests.get(channel_url)
-    if response.status_code == 200:
-        return response.text
-    return None
+    vless_links = []
+    vmess_links = []
+    ss_links = []
+
+    for link in channel_url:
+        response = requests.get(link)
+        vless_links.extend(re.findall(r'vless://[^\s]+', response.text))
+        vmess_links.extend(re.findall(r'vmess://[^\s]+', response.text))
+        ss_links.extend(re.findall(r'ss://[^\s]+', response.text))
+
+    return vless_links,vmess_links,ss_links
 
 def extract_links(content):
     vless_links = []
     vmess_links = []
     ss_links = []
-    vless_links = re.findall(r'vless://[^\s]+', content)
-    vmess_links = re.findall(r'vmess://[^\s]+', content)
-    ss_links = re.findall(r'ss://[^\s]+', content)
+    for link in content:
+        vless_links.extend(re.findall(r'vless://[^\s]+', link))
+        vmess_links.extend(re.findall(r'vmess://[^\s]+', link))
+        ss_links.extend(re.findall(r'ss://[^\s]+', link))
     return vless_links, vmess_links, ss_links
 
 def save_links_to_file(vless_links, vmess_links, ss_links, filename):
@@ -34,10 +42,10 @@ def save_links_to_file(vless_links, vmess_links, ss_links, filename):
             file.write(link + "\n")
 
 def main():
-    channel_url = 'https://t.me/s/IP_CF_Config'
+    channel_url  = ['https://t.me/s/IP_CF_Config','https://t.me/s/nufilter']
     content = fetch_v2ray_links(channel_url)
     if content:
-        vless_links, vmess_links, ss_links =  extract_links(content)
+        vless_links, vmess_links, ss_links =  content
 
 
 
@@ -48,7 +56,21 @@ def main():
     
         file1.write("VLESS Link:\n")
         for vle in vless_links:
-            file1.writelines(vle[:-2].replace("amp;", "").replace("#", "#IP_CF\n"))
+            file1.writelines(vle[:-2].replace("amp;", "")
+                            .replace('%40NUFiLTER+%F0%9F%87%A9%F0%9F%87%AA+ws</code><br/><br/><tg-emo', "")
+                            .replace('%40NUFiLTER+%F0%9F%87%A8%F0%9F%87%AD+ws2</code><br/><br/><tg-emo', "")
+                            .replace('%40NUFiLTER+%F0%9F%87%A9%F0%9F%87%AA+ws3</code><br/><br/><tg-emo', "")
+                            .replace('%40NUFiLTER+%F0%9F%87%A8%F0%9F%87%AD+ws4</code><br/><br/><tg-emo', "")
+                            .replace('%40NUFiLTER+%F0%9F%87%A9%F0%9F%87%AA+ws5</code><br/><br/><tg-emo', "")
+                            .replace('%40NUFiLTER+%F0%9F%87%A9%F0%9F%87%AA+ws6</code><br/><br/><tg-emo', "")
+                            .replace('%40NUFiLTER+%F0%9F%87%A9%F0%9F%87%AA+ws2</code><br/><br/><tg-emo', "")
+                            .replace('%40NUFiLTER+%F0%9F%87%A8%F0%9F%87%AD+ws3</code><br/><br/><tg-emo', "")
+                            .replace('%40NUFiLTER+%F0%9F%87%A8%F0%9F%87%AD+ws</code><br/><br/><tg-emo', "")
+                            .replace('%40NUFiLTER+%F0%9F%87%A6%F0%9F%87%B9+ws5<br/><br/>', "")
+                            .replace('%40NUFiLTER+%F0%9F%87%A9%F0%9F%87%AA+ws4</code><br/><br/><tg-emo', "")
+                            .replace('%40NUFiLTER+%F0%9F%87%B3%F0%9F%87%B1+ws</code><br/><br/><tg-emo', "")
+                            .replace('%40NUFiLTER+%F0%9F%87%A6%F0%9F%87%B9ws5</code><br/><br/><tg-emo', "")
+                            .replace("#", "#IP_CF\n"))
             print(file1) 
             
 
