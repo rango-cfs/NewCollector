10ed8903001ecbcd4cb5ba65b5617db4533ab667
Rango_CF <118201907+rango-cfs@users.noreply.github.com>
2025-01-24 02:05:22 +0330

    Update main.py

--- main.py
+++ main.py
@@ -6,27 +6,27 @@ import re
 def fetch_v2ray_links(channel_url):
     vless_links = []
     vmess_links = []
-    ss_links = []
+    # ss_links = []
 
     for link in channel_url:
         response = requests.get(link)
         vless_links.extend(re.findall(r'vless://[^\s]+', response.text))
         vmess_links.extend(re.findall(r'vmess://[^\s]+', response.text))
-        ss_links.extend(re.findall(r'ss://[^\s]+', response.text))
+        # ss_links.extend(re.findall(r'ss://[^\s]+', response.text))
 
-    return vless_links,vmess_links,ss_links
+    return vless_links,vmess_links
 
 def extract_links(content):
     vless_links = []
     vmess_links = []
-    ss_links = []
+    # ss_links = []
     for link in content:
         vless_links.extend(re.findall(r'vless://[^\s]+', link))
         vmess_links.extend(re.findall(r'vmess://[^\s]+', link))
-        ss_links.extend(re.findall(r'ss://[^\s]+', link))
-    return vless_links, vmess_links, ss_links
+        # ss_links.extend(re.findall(r'ss://[^\s]+', link))
+    return vless_links, vmess_links
 
-def save_links_to_file(vless_links, vmess_links, ss_links, filename):
+def save_links_to_file(vless_links, vmess_links, filename):
     with open(filename, 'w') as file:
         
         file.write("VLESS Links:\n")
@@ -37,15 +37,12 @@ def save_links_to_file(vless_links, vmess_links, ss_links, filename):
         for link in vmess_links:
             file.write(link + "\n")
 
-        file.write("\nSS Links:\n")
-        for link in ss_links:
-            file.write(link + "\n")
 
 def main():
     channel_url  = ['https://t.me/s/IP_CF_Config','https://t.me/s/nufilter']
     content = fetch_v2ray_links(channel_url)
     if content:
-        vless_links, vmess_links, ss_links =  content
+        vless_links, vmess_links =  content
 
 
 
@@ -83,34 +80,34 @@ def main():
             print(file1) 
         
         
-        file1.write("\n")
-        file1.write("SS Link:\n")
-        for vme in ss_links:
-            file1.writelines(vme[:-2].replace("amp;", "")
-                             .replace("</code><br/><br/>•••••••••••••••••••••••••••••••••••<br/>", "\n")
-                             .replace("%40NUFiLTER+%F0%9F%87%A8%F0%9F%87%AD+ws4</code><br/><br/><tg-emo", "")
-                             .replace("%40NUFiLTER+%F0%9F%87%A9%F0%9F%87%AA+ws5</code><br/><br/><tg-emo", "")
-                             .replace("%40NUFiLTER+%F0%9F%87%A8%F0%9F%87%AD+ws</code><br/><br/><tg-emo", "")
-                             .replace("%40NUFiLTER+%F0%9F%87%A9%F0%9F%87%AA+ws2</code><br/><br/><tg-emo", "")
-                             .replace("%40NUFiLTER+%F0%9F%87%A8%F0%9F%87%AD+ws3</code><br/><br/><tg-emo", "")
-                             .replace("%40NUFiLTER+%F0%9F%87%A6%F0%9F%87%B9+ws5<br/><br/>", "")
-                             .replace("%40NUFiLTER+%F0%9F%87%A8%F0%9F%87%AD+ws4</code><br/><br/><tg-emo", "")
-                             .replace("%40NUFiLTER+%F0%9F%87%A9%F0%9F%87%AA+ws6</code><br/><br/><tg-emo", "")
-                             .replace("%40NUFiLTER+%F0%9F%87%A8%F0%9F%87%AD+ws</code><br/><br/><tg-emo", "")
-                             .replace("%40NUFiLTER+%F0%9F%87%A9%F0%9F%87%AA+ws2</code><br/><br/><tg-emo", "")
-                             .replace("%40NUFiLTER+%F0%9F%87%A8%F0%9F%87%AD+ws3</code><br/><br/><tg-emo", "")
-                             .replace("%40NUFiLTER+%F0%9F%87%A9%F0%9F%87%AA+ws4</code><br/><br/><tg-emo", "")
-                             .replace("%40NUFiLTER+%F0%9F%87%B3%F0%9F%87%B1+ws</code><br/><br/><tg-emo", "")
-                             .replace("%40NUFiLTER+%F0%9F%87%A9%F0%9F%87%AA+ws2</code><br/><br/><tg-emo", "")
-                             .replace("%40NUFiLTER+%F0%9F%87%A9%F0%9F%87%AA+ws3</code><br/><br/><tg-emo", "")
-                             .replace("%40NUFiLTER+%F0%9F%87%A9%F0%9F%87%AA+ws4</code><br/><br/><tg-emo", "")
-                             .replace("%40NUFiLTER+%F0%9F%87%A6%F0%9F%87%B9ws5</code><br/><br/><tg-emo", "")
-                             .replace("%40NUFiLTER+%F0%9F%87%A9%F0%9F%87%AA+ws6</code><br/><br/><tg-emo", "")
-                             .replace("%40NUFiLTER+%F0%9F%87%A9%F0%9F%87%AA+ws7</code><br/><br/><tg-emo", "")
-                             .replace("%40NUFiLTER+%F0%9F%87%A9%F0%9F%87%AA+ws</code><br/><br/><tg-emo", "")
-                             .replace("%40NUFiLTER+%F0%9F%87%A9%F0%9F%87%AA+ws2</code><br/><br/><tg-emo", "")
-                             .replace("%40NUFiLTER+%F0%9F%87%A9%F0%9F%87%AA+TLS</code><br/><br/><tg-emo", "")
-                             .replace("#", "#IP_CF\n"))
+        # file1.write("\n")
+        # file1.write("SS Link:\n")
+        # for vme in ss_links:
+        #     file1.writelines(vme[:-2].replace("amp;", "")
+        #                      .replace("</code><br/><br/>•••••••••••••••••••••••••••••••••••<br/>", "\n")
+        #                      .replace("%40NUFiLTER+%F0%9F%87%A8%F0%9F%87%AD+ws4</code><br/><br/><tg-emo", "")
+        #                      .replace("%40NUFiLTER+%F0%9F%87%A9%F0%9F%87%AA+ws5</code><br/><br/><tg-emo", "")
+        #                      .replace("%40NUFiLTER+%F0%9F%87%A8%F0%9F%87%AD+ws</code><br/><br/><tg-emo", "")
+        #                      .replace("%40NUFiLTER+%F0%9F%87%A9%F0%9F%87%AA+ws2</code><br/><br/><tg-emo", "")
+        #                      .replace("%40NUFiLTER+%F0%9F%87%A8%F0%9F%87%AD+ws3</code><br/><br/><tg-emo", "")
+        #                      .replace("%40NUFiLTER+%F0%9F%87%A6%F0%9F%87%B9+ws5<br/><br/>", "")
+        #                      .replace("%40NUFiLTER+%F0%9F%87%A8%F0%9F%87%AD+ws4</code><br/><br/><tg-emo", "")
+        #                      .replace("%40NUFiLTER+%F0%9F%87%A9%F0%9F%87%AA+ws6</code><br/><br/><tg-emo", "")
+        #                      .replace("%40NUFiLTER+%F0%9F%87%A8%F0%9F%87%AD+ws</code><br/><br/><tg-emo", "")
+        #                      .replace("%40NUFiLTER+%F0%9F%87%A9%F0%9F%87%AA+ws2</code><br/><br/><tg-emo", "")
+        #                      .replace("%40NUFiLTER+%F0%9F%87%A8%F0%9F%87%AD+ws3</code><br/><br/><tg-emo", "")
+        #                      .replace("%40NUFiLTER+%F0%9F%87%A9%F0%9F%87%AA+ws4</code><br/><br/><tg-emo", "")
+        #                      .replace("%40NUFiLTER+%F0%9F%87%B3%F0%9F%87%B1+ws</code><br/><br/><tg-emo", "")
+        #                      .replace("%40NUFiLTER+%F0%9F%87%A9%F0%9F%87%AA+ws2</code><br/><br/><tg-emo", "")
+        #                      .replace("%40NUFiLTER+%F0%9F%87%A9%F0%9F%87%AA+ws3</code><br/><br/><tg-emo", "")
+        #                      .replace("%40NUFiLTER+%F0%9F%87%A9%F0%9F%87%AA+ws4</code><br/><br/><tg-emo", "")
+        #                      .replace("%40NUFiLTER+%F0%9F%87%A6%F0%9F%87%B9ws5</code><br/><br/><tg-emo", "")
+        #                      .replace("%40NUFiLTER+%F0%9F%87%A9%F0%9F%87%AA+ws6</code><br/><br/><tg-emo", "")
+        #                      .replace("%40NUFiLTER+%F0%9F%87%A9%F0%9F%87%AA+ws7</code><br/><br/><tg-emo", "")
+        #                      .replace("%40NUFiLTER+%F0%9F%87%A9%F0%9F%87%AA+ws</code><br/><br/><tg-emo", "")
+        #                      .replace("%40NUFiLTER+%F0%9F%87%A9%F0%9F%87%AA+ws2</code><br/><br/><tg-emo", "")
+        #                      .replace("%40NUFiLTER+%F0%9F%87%A9%F0%9F%87%AA+TLS</code><br/><br/><tg-emo", "")
+        #                      .replace("#", "#IP_CF\n"))
             print(file1) 
             
         file1.close()
